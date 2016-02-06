d0783b4f99be63bc842e20bd820a9034e89e1f12
Tommy PAGEARD <tpageard@e2r13p13.42.fr>
2016-02-06 12:25:42 +0100

    add rooting for reset_password

--- private/rooting.php
+++ private/rooting.php
@@ -11,24 +11,36 @@
 			case 'connexion':
 				include_once $path . '/public/view/connexion.html';
 				break;
+			case 'reset_password':
+				include_once $path . '/public/view/lost_password.html';
+				break;
 			default:
 				include_once $path . '/public/view/404.html';
 				break;
 		}
 	}
-	else if( !empty($_GET['p']) && $_GET['p'] == 'inscription' && !empty($_GET['t']) ) {
+	else if( !empty($_GET['p']) && !empty($_GET['t']) ) {
+		switch ( $_GET['p'] ) {
+			case 'inscription':
+				$token_email = $_GET['t'];
 
-		$token_email = $_GET['t'];
-
-		$update = 'UPDATE users SET active = 1 WHERE token_email = :token_email';
-		$mysql = $db->prepare($update);
-		$mysql->bindValue(':token_email', $token_email);
-		$mysql->execute();
-		if( $mysql->rowCount() == 1 ) {
-			include_once $path . '/public/view/valide_account.html';
+				$update = 'UPDATE users SET active = 1 WHERE token_email = :token_email';
+				$mysql = $db->prepare($update);
+				$mysql->bindValue(':token_email', $token_email);
+				$mysql->execute();
+				if( $mysql->rowCount() == 1 ) {
+					include_once $path . '/public/view/valide_account.html';
+				}
+				else
+					include_once $path . '/public/view/404.html';
+				break;
+			case 'reset_password':
+				include_once $path . '/public/view/change_password.html';
+				break;
+			default:
+				include_once $path . '/public/view/404.html';
+				break;
 		}
-		else
-			include_once $path . '/public/view/404.html';
 	}
 	else {
 		include_once $path . '/public/view/index.html';
