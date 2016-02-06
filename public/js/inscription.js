1108c286f97e949f6b1d5d0b71c803d16df418ba
Tommy PAGEARD <tpageard@e2r13p13.42.fr>
2016-02-06 17:01:16 +0100

    adding forgotten semi colon and mistake text

--- public/js/inscription.js
+++ public/js/inscription.js
@@ -8,9 +8,9 @@ document.addEventListener('DOMContentLoaded', function(){
 		event.preventDefault();
 
 		// get information
-		var username = document.getElementsByClassName("username-input")[0].value
-		var email = document.getElementsByClassName("email-input")[0].value
-		var password = document.getElementsByClassName("password-input")[0].value
+		var username = document.getElementsByClassName("username-input")[0].value;
+		var email = document.getElementsByClassName("email-input")[0].value;
+		var password = document.getElementsByClassName("password-input")[0].value;
 
 		var send = 'inscription=1&username='+ encodeURIComponent(username) +
 			'&email='+ encodeURIComponent(email) +
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function(){
 				console.log(result);
 				if( result == 'success' ) {
 					document.getElementsByClassName("p-success")[0]
-						.innerHTML = "<h3>Inscription Validé.<br>Veuillez Activer votre compte<br>en cliquant sur le lien dans l'email</h3>";
+						.innerHTML = "<h3>Inscription validé.<br>Veuillez Activer votre compte<br>en cliquant sur le lien dans l'email</h3>";
 				}
 				else {
 					if( Array.isArray(result) ) {
