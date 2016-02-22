3d9a00af90c90bd72ae47f86a24bb9ad00b1f28e
Tommy PAGEARD <tpageard@e2r13p13.42.fr>
2016-02-22 19:47:35 +0100

    check valid user_pic and filter

--- api/montage.php
+++ api/montage.php
@@ -7,13 +7,30 @@
 	*/
 	if( !empty($_POST['save_img']) ) {
 
+		$error = array();
+
 		$remove = array( ' ', 'data:image/png;base64,' );
 		$replace = array( '+', '' );
 		$user_pic = str_replace($remove, $replace, $_POST['user_pic']);
-		if ( check_base64_image($user_pic) ) 
-			echo ' IMG';
+
+		if( empty($user_pic) )
+			$error[] = 'Image utilisateur non valable';
+
+		$f_url = 'https://e2r13p13:8443/camagru/public/img/pool/filter5.png';
+		preg_match('/[^\/]+$/', $f_url, $img);
+		if( !empty($img[0]) )
+			$filter = $path . '/public/img/pool/' . $img[0];
 		else
-			echo ' NOT IMG';
+			$error[] = 'Mauvais filtre.';
+		if( empty($error) ) {
+			if( check_base64_image($user_pic) && file_exists($filter) )
+				echo ' IMG';
+			else
+				echo ' NOT IMG';
+		}
+		else
+			die(json_encode($error));
 	}
+
 
 ?>
