b0d12645b7be4321001589820a7a561c011df004
Tommy PAGEARD <tpageard@e2r13p13.42.fr>
2016-02-22 19:27:28 +0100

    add function to check if img is real img

--- lib/functions.php
+++ lib/functions.php
@@ -13,4 +13,25 @@ function is_log() {
 	else
 		return FALSE;
 }
+
+function check_base64_image($base64) {
+
+
+    $img = imagecreatefromstring(base64_decode($base64));
+    if (!$img) {
+        return false;
+    }
+    $name = hash('whirlpool', 'tmp') . '.png';
+    
+    imagepng($img, $name);
+    $info = getimagesize($name);
+
+    unlink($name);
+
+    if ($info[0] > 0 && $info[1] > 0 && $info['mime']) {
+        return true;
+    }
+    return false;
+}
+
 ?>
