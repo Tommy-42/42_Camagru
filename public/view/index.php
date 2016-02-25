b366c6fe29cc21e8f6a3baa3a5379c569e46c654
Tommy PAGEARD <tpageard@e2r13p13.42.fr>
2016-02-26 00:41:01 +0100

    modal update

--- public/view/index.php
+++ public/view/index.php
@@ -38,11 +38,30 @@
 	</div>
 </div>
 <div id="modal">
-	<span class="icon delete-icon text-red modal-dismiss" onclick="hideModal()"></span>
-	<div class="modal-box">
-		<img class="modal-preview-img" src="">
-		<div class="likes"></div>
-		<div class="comments"></div>
+	<div class="row">
+		<div class="col-12 no-padding-at-all"></div>
+			<span id="modal-dismiss" class="icon delete-icon text-red" onclick="hideModal()"></span>
+		</div>
+		<div class="col-12 no-padding-at-xs">
+			<div id="modal-box">
+				<div id="tumbnail-img" class="no-padding-at-all">	
+					<img id="modal-preview-img" src="" data-id="">
+					<div id="img-infos">
+						<div class="col-4 no-padding-at-all">
+							<span class="pull-left"><span id="like-number">0</span> Like(s)</span>
+							<span id="like-img-btn" onclick="likeImg()" class="icon like-icon"></span>
+						</div>
+						<div class="col-8 no-padding-at-all">
+							<?php if( is_log() ) { ?>
+								<input type="text" placeholder="Ajouter un com !" value="">
+							<?php } ?>
+						</div>
+					</div>
+				</div>
+				<div id="likes"></div>
+				<div id="comments"></div>
+			</div>
+		</div>
 	</div>
 </div>
 <script src="public/js/galerie.js" type="text/javascript"></script>
