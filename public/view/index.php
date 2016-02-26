e55a6edeb27de1de0990816795593b6cbd2f816f
Tommy PAGEARD <tpageard@e2r13p13.42.fr>
2016-02-26 17:28:06 +0100

    check if galerie is not empty

--- public/view/index.php
+++ public/view/index.php
@@ -3,36 +3,38 @@
 		<h1 class="text-uppercase">Bienvenue</h1>
 		<div class="galerie col-12">
 		<?php
-			$i = 0;
-			$page = 0;
 			$imgs = getAllUsersImgs();
-			$count = sizeof($imgs);
-			echo '<div class="page-container col-12">';
-			while( $i < $count ) {
-				if( $i % 11 == 1 && $i != 1 ) {
-					echo '</div>';
-					echo '<div class="page-container-count col-12"><span class="page-left" onclick="pageLeft(this)"><</span>' . $page . '<span class="page-right" onclick="pageRight(this)">></span></div>';
-					echo '<div class="page-container col-12">';
-					echo '<div class="col-3 col-xs-6">
-							<img onclick="imgInfo(this)" class="galerie-img" src="private/galerie/' 
-								. $imgs[$i]['name']. '.png" 
-								data-id="'. $imgs[$i]['id'] .'">
-						</div>
-					';
-					$page++;
+			if( !empty($imgs) ) {
+				$i = 0;
+				$page = 0;
+				$count = sizeof($imgs);
+				echo '<div class="page-container col-12">';
+				while( $i < $count ) {
+					if( $i % 11 == 1 && $i != 1 ) {
+						echo '</div>';
+						echo '<div class="page-container-count col-12"><span class="page-left" onclick="pageLeft(this)"><</span>' . $page . '<span class="page-right" onclick="pageRight(this)">></span></div>';
+						echo '<div class="page-container col-12">';
+						echo '<div class="col-3 col-xs-6">
+								<img onclick="imgInfo(this)" class="galerie-img" src="private/galerie/' 
+									. $imgs[$i]['name']. '.png" 
+									data-id="'. $imgs[$i]['id'] .'">
+							</div>
+						';
+						$page++;
+					}
+					else {
+						echo '<div class="col-3 col-xs-6">
+								<img onclick="imgInfo(this)" class="galerie-img" src="private/galerie/' 
+									. $imgs[$i]['name']. '.png" 
+									data-id="'. $imgs[$i]['id'] .'">
+							</div>
+						';	
+					}
+					$i++;
 				}
-				else {
-					echo '<div class="col-3 col-xs-6">
-							<img onclick="imgInfo(this)" class="galerie-img" src="private/galerie/' 
-								. $imgs[$i]['name']. '.png" 
-								data-id="'. $imgs[$i]['id'] .'">
-						</div>
-					';	
-				}
-				$i++;
+				echo '</div>';
+				echo '<div class="page-container-count col-12"><span class="page-left" onclick="pageLeft(this)"><</span>' . $page . '<span class="page-right" onclick="pageRight(this)">></span></div>';
 			}
-			echo '</div>';
-			echo '<div class="page-container-count col-12"><span class="page-left" onclick="pageLeft(this)"><</span>' . $page . '<span class="page-right" onclick="pageRight(this)">></span></div>';
 		?>
 		</div>
 	</div>
