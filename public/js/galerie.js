d65ff8d1ef834be8bbfab91250a0f06a3baa4307
Tommy PAGEARD <tpageard@e2r13p13.42.fr>
2016-02-26 04:01:51 +0100

    show comments, get comments ok

--- public/js/galerie.js
+++ public/js/galerie.js
@@ -1,13 +1,9 @@
-document.addEventListener('DOMContentLoaded', function(){ 
-
-});
 function imgInfo(el) {
 
 	resetModal();
 
 	var modal = document.getElementById('modal-box');
 	var img = document.getElementById('modal-preview-img');
-	var likes = document.getElementById('likes');
 	var like_number = document.getElementById('like-number');
 	var comments = document.getElementById('comments');
 
@@ -22,19 +18,33 @@ function imgInfo(el) {
 			var result = JSON.parse(xhttp.responseText);
 			console.log(result);
 			if( result.hasOwnProperty('success' ) ) {
+				// <script>console.log('toto');</script>
 				var data = result.success;
 				img.src = 'private/galerie/' + data['infos']['name'] + '.png';
 				img.setAttribute('data-id', data['infos']['id']);
 				like_number.innerHTML = data['likes']['total'];
-				if( data['likes']['is_liked'] != 0 ) {
-					document.getElementById('like-img-btn').classList.remove('like-icon');
-					document.getElementById('like-img-btn').classList.add('liked-icon');
+				if( document.getElementById('like-img-btn') !== null ) {
+					if( data['likes']['is_liked'] != 0 ) {
+						document.getElementById('like-img-btn').classList.remove('like-icon');
+						document.getElementById('like-img-btn').classList.add('liked-icon');
+					}
+					else {
+						document.getElementById('like-img-btn').classList.remove('liked-icon');
+						document.getElementById('like-img-btn').classList.add('like-icon');
+					}
 				}
-				else {
-					document.getElementById('like-img-btn').classList.remove('liked-icon');
-					document.getElementById('like-img-btn').classList.add('like-icon');
-				}
-
+				var html = '';
+				for ( var i = 0; i < data['comments'].length; i++ ) {
+					html += '<div class="com-box col-12 no-padding-at-all">' +
+							'<p>\'\'' +
+								data['comments'][i]['comment'] +
+								'\'\'<br><small class="com-username text-right">' +
+									data['comments'][i]['username'] +
+								'</small>' +
+							'</p>' +
+						'</div>';
+				};
+				comments.innerHTML = html;
 			}
 			else {
 				// console.log( xhttp.responseText );
@@ -82,6 +92,74 @@ function likeImg() {
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhttp.send(send);
 }
+function commentImg(el) {
+
+    var key = event.which || event.keyCode;
+    if (key === 13) { // 13 is enter
+		var comment = el.value;
+		if( comment.length > 140 ) {
+			alert('Msg trop long, 140 caracteres maximum');
+			return;
+		}
+		else {
+
+			var img_id = document.getElementById('modal-preview-img').getAttribute('data-id');
+			var send = 'post_comment=1&img_id=' + img_id + '&comment=' + encodeURIComponent(comment);
+			// create AJAX ressources
+			var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (xhttp.readyState == 4 && xhttp.status == 200) {
+					var result = JSON.parse(xhttp.responseText);
+					if( result == 'success' ) {
+						el.value = '';
+						var comments = document.getElementById('comments');
+						comments.innerHTML = reloadComments() || '';
+					}
+					else {
+						
+					}
+				}
+			};
+			xhttp.open("POST", "api/galerie.php", false);
+			xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhttp.send(send);
+		}
+    }
+}
+function reloadComments() {
+
+	var modal = document.getElementById('modal-box');
+	var img = document.getElementById('modal-preview-img');
+	var comments = document.getElementById('comments');
+
+	var img_id = img.getAttribute('data-id');
+	var send = 'get_comments=1&img_id=' + img_id;
+
+	// create AJAX ressources
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("POST", "api/galerie.php", false);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send(send);
+	var result = JSON.parse(xhttp.responseText);
+	if( result.hasOwnProperty('success' ) ) {
+		var data = result.success;
+		var html = '';
+		for ( var i = 0; i < data['comments'].length; i++ ) {
+			html += '<div class="com-box col-12 no-padding-at-all">' +
+					'<p>\'\'' +
+						data['comments'][i]['comment'] +
+						'\'\'<br><small class="com-username text-right">' +
+							data['comments'][i]['username'] +
+						'</small>' +
+					'</p>' +
+				'</div>';
+		};
+		return(html);
+	}
+	else {
+		return false;
+	}
+}
 function showModal() {
 	document.getElementById('modal').style.display = 'block';
 	document.body.innerHTML = '<div id="modal-is-on"></div>' +
@@ -91,15 +169,14 @@ function resetModal() {
 
 	var modal = document.getElementById('modal-box');
 	var img = document.getElementById('modal-preview-img');
-	var likes = document.getElementById('likes');
 	var comments = document.getElementById('comments');
-	var like_img_btn = document.getElementById('like-img-btn');
 
 	img.src = '';
-	likes.innerHTML = '';
-	comments.innerHTML = '';
-	like_img_btn.classList.remove('liked-icon');
-	like_img_btn.classList.add('like-icon');
+	//comments.innerHTML = '';
+	if( document.getElementById('like-img-btn') !== null ) {
+		document.getElementById('like-img-btn').classList.remove('liked-icon');
+		document.getElementById('like-img-btn').classList.add('like-icon');
+	}
 }
 function hideModal() {
 	document.getElementById('modal').style.display = 'none';
@@ -108,11 +185,9 @@ function hideModal() {
 
 	var modal = document.getElementById('modal-box');
 	var img = document.getElementById('modal-preview-img');
-	var likes = document.getElementById('likes');
 	var comments = document.getElementById('comments');
 
 	img.src = '';
-	likes.innerHTML = '';
 	comments.innerHTML = '';
 } 
 function pageLeft(el) {
