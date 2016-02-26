2df22fe87679f6d7f5c5ea6b8cf9f4f6bb3022eb
Tommy PAGEARD <tpageard@e2r13p13.42.fr>
2016-02-26 17:28:21 +0100

    update config

--- config/database.php
+++ config/database.php
@@ -1,11 +1,16 @@
 <?php
 	$DB_HOST = "127.0.0.1:3306";
+	
 	$DB_TYPE = "mysql";
 	$DB_CHARSET = "UTF8";
 	$DB_OPTIONS = array(PDO::ERRMODE_EXCEPTION => true);
+	
 	$DB_NAME = "camagru"; 	
 	$DB_USER = "camagru";
 	$DB_PASSWORD = "toto42";
+
+	$DB_ROOT = "root";
+	$DB_P_ROOT = "therootpassword";
 
 	$DB_DSN = $DB_TYPE . ":host=" . $DB_HOST . ";dbname=" . $DB_NAME . ";charset=" . $DB_CHARSET;
 ?>
